d6596d7f27faba840ac2c3468e56ab00c2ec5ee9
BojanMarkovic659 <markovicc.b@gmail.com>
2023-02-27 17:11:03 +0100

    initial commit

---
+++ src/main/java/com/artistsolutions/betting/TournamentBettingApplication.java
@@ -0,0 +1,13 @@
+package com.artistsolutions.betting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamentBettingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournamentBettingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artistsolutions/betting/TournamentBettingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artistsolutions.betting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournamentBettingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
