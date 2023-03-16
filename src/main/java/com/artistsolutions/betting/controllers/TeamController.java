d793010caa539c7a3b549885330286838a691a36
BojanMarkovic659 <markovicc.b@gmail.com>
2023-03-16 13:41:20 +0100

    Tournament betting (#1)
    * Created team controller

---
+++ src/main/java/com/artistsolutions/betting/controllers/TeamController.java
@@ -0,0 +1,29 @@
+package com.artistsolutions.betting.controllers;
+
+import com.artistsolutions.betting.dto.Team;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/teams")
+public class TeamController {
+
+    @GetMapping()
+    public void getTeams() {
+        System.out.println("All teams returned");
+    }
+
+    @PostMapping("/add")
+    public void addTeams(@RequestBody Team request) {
+        System.out.println("Team successfully added: " + request.getName() + ", id: " + request.getId());
+    }
+
+    @PutMapping("/{id}")
+    public void updateTeam(@PathVariable int id, @RequestBody Team request) {
+        System.out.println("Team successfully update, id: " + id + ", name: " + request.getName());
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTeam(@PathVariable int id) {
+        System.out.println("Team is successfully deleted, id: " + id);
+    }
+}
